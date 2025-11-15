07b7aa7ff5940f622aeebc6093f5ae22c28991a9
mdh.danial <mdh.danialhaziq@gmail.com>
2025-11-15 20:15:18 +0800

    added error handling

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ if response.status_code == 200:
     # 4. convert response frpm JSON to Python dict
     data = response.json()
     print("Data received", data)
-
 elif response.status_code == 404:
-    print("username not found, ERROR:  ", response.status_code)
+    print("username not found, ERROR:", response.status_code)
+elif response.status_code == 400:
+    print("request was malformed, ERROR:", response.status_code)
+elif response.status_code == 401:
+    print("invalid/missing API key or token, ERROR:", response.status_code)
+elif response.status_code == 403:
+    print("resource access is denied, ERROR:", response.status_code)
+elif response.status_code == 405:
+    print ("method not allowed, ERROR:", response.status_code)
+elif response.status_code == 429:
+    print("rate limit exceeded. ERROR:", response.status_code)
+else:
+    print("ERROR:", response.status_code)
